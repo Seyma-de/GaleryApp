b5f239ce5c4c7bc339d8ca6bf656fcaf66a4a834
seyma <betul.cagil.krdmn@gmail.com>
2023-10-12 21:34:36 +0200

    add

---
+++ src/Images.js
@@ -0,0 +1,9 @@
+export default [
+  "https://content.skyscnr.com/m/6d02f5de00f23cfc/original/GettyImages-464617079_doc.jpg?crop=1224px:647px&quality=100&position=attention",
+  "https://content.skyscnr.com/m/7d9ad1092faa7a96/original/eiffel-tower-day.jpg?resize=800px:600px&quality=100",
+  "https://content.skyscnr.com/m/39324a095cdf79f8/original/rome-182281845.jpg?resize=800px:600px&quality=100",
+  "https://content.skyscnr.com/m/7b8f32e1723b2a2c/original/GettyImages-471070086.jpg?resize=1800px:1800px&quality=100",
+  "https://content.skyscnr.com/m/6d02f5de00f23cfc/original/GettyImages-464617079_doc.jpg?resize=1800px:1800px&quality=100",
+  "https://content.skyscnr.com/m/16d0e6ed6a64fafb/original/GettyImages-147454466_doc.jpg?resize=1800px:1800px&quality=100",
+  "https://content.skyscnr.com/m/19bdb7404e8a08ef/original/madrid.jpg?crop=1224px:647px&quality=100&position=attention",
+];
