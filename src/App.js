fd61faf5970450abda2f967692611156bd163e10
seyma <betul.cagil.krdmn@gmail.com>
2023-10-12 21:40:02 +0200

    add title

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1>My Favorite Cities</h1>
       <div className="container">
         <img src={selectedImg} alt="Selected" className="selected" />
       </div>
